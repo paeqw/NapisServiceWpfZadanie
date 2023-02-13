afb9efa66c7f1a576da36c269f645a448500ab2d
01_3BT_2 <01_3BT_2@DESKTOP-A8U3OEL>
2023-02-13 12:16:45 +0100

    Add project files.

---
+++ ClassLibrary1/NapisService.cs
@@ -0,0 +1,50 @@
+﻿namespace ClassLibrary1 { 
+    public class NapisService : INapisService
+    {
+        private String napis;
+
+        public NapisService(string napis)
+        {
+            this.napis = napis;
+        }
+        private string remoSpaces(string s)
+        {
+            String aa = "";
+            char[] chars = s.ToCharArray();
+            foreach (var el in chars)
+            {
+                if (el != ' ') aa += el.ToString();
+            }
+            return aa;
+        }
+        private string odwroc(string doOdwrocenia)
+        {
+            char[] a = doOdwrocenia.ToCharArray();
+            char[] wyjscie = new char[a.Length];
+            for (int i = 0; i < a.Length; i++)
+            {
+                wyjscie[a.Length - 1 - i] = a[i];
+            }
+            String aa = "";
+            foreach (var el in wyjscie) {
+                if (el != ' ') aa += el.ToString();
+            }
+            return aa;
+        }
+
+        public bool CzyPalindrom()
+        {   
+            
+            return (remoSpaces(this.napis) == odwroc(this.napis));
+        }
+        
+
+        public int ileLiterWNapisie(char znak)
+        {
+            char[] a = this.napis.ToCharArray();
+            int c = 0;
+            foreach (var el in a) if (el == znak) c++;
+            return c;
+        }
+    }
+}
---
+++ WpfApp7/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using ClassLibrary1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp7
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void wykonaj_Click(object sender, RoutedEventArgs e)
+        {
+            INapisService napisService;
+            if (jakiChar.Text != "")
+            {
+                napisService = new NapisService(textDoOperacji.Text);
+                int ileLiter = napisService.ileLiterWNapisie(Convert.ToChar(jakiChar.Text));
+                bool czyPalindrom = napisService.CzyPalindrom();
+                String pal = "";
+                if (czyPalindrom) pal = $"Napis {textDoOperacji.Text} jest palindromem. Litery {jakiChar.Text} w tekscie jest {ileLiter} razy";
+                else pal = $"Napis {textDoOperacji.Text} nie jest palindromem. Litery {jakiChar.Text} w tekscie jest {ileLiter} razy";
+                output.Text = pal;
+            }
+            else
+            {
+                NapisService a = new NapisService(textDoOperacji.Text);
+                napisService = new NapisService(textDoOperacji.Text);
+                bool czyPalindrom = napisService.CzyPalindrom();
+                String pal = "";
+                if (czyPalindrom) pal = $"Napis {textDoOperacji.Text} jest palindromem.";
+                else pal = $"Napis {textDoOperacji.Text} nie jest palindromem.";
+                output.Text = pal;
+            }
+        }
+    }
+}
